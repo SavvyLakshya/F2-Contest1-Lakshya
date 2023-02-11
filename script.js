e83e182d386ff11fec64e00120e80ec16184d15a
SavvyLakshya <117033261+SavvyLakshya@users.noreply.github.com>
2023-02-11 16:55:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map(a => {
+    if(a.marks>50){
+      console.log(a);
+    }
+   })
+
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(student =>{
+    if(student.marks>50){
+      console.log(student);
+    }
+  } )
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudnet = {id:4,name:"susan",age:"20",marks:45}
+  arr.push(newStudnet)
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr= arr.filter(student =>student.marks >=50)
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 5, name: "Lakshya", age: "24", marks: 89 },
+    { id: 6, name: "Kola", age: "24", marks: 90},
+    { id: 7, name: "Komal", age: "23", marks: 92 },
+  ];
+ let result= arr.concat(arr1)
+  console.log(result);
 }
